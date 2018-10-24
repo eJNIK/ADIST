6aa4e34e089e4b38ecd68e6e4101e3c922aff67c
Jakub Ejnik <jakub.ejnik@gmail.com>
2018-10-24 20:03:02 +0200

    wersja finalna 1.0

--- adist.py
+++ adist.py
@@ -2,7 +2,6 @@ from PyQt5.QtWidgets import QApplication
 from Tools import argv, exit
 from MainWindow import MainWindow
 
-
 if __name__ == '__main__':
     app = QApplication(argv)
     w = MainWindow()
